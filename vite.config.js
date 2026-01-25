1c4a92500b1515d34e4be48b28e451a492bb8601
valera-goippro <valera@gsm-travel.com>
2026-01-25 02:10:31 +0000

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   root: '.',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'),
+      }
+    }
   }
 })
